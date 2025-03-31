d527c73b694aa7c8c16d6e58856992d9a26f2317
AlfredoFrontera <alfredo.frontera2@upr.edu>
2025-03-31 02:01:23 +0000

    commit final

--- HW6.cpp
+++ HW6.cpp
@@ -37,3 +37,83 @@ float area(float largo, float ancho, float alto) {
 float area(float radio, float altura, bool esCilindro) {
     return 2 * M_PI * radio * (radio + altura);
 }
+
+// Función principal
+int main() {
+    cout << fixed << setprecision(2); // Mostrar resultados con 2 decimales
+
+    cout << "Este programa calcula el volumen y el área de la superficie de tres figuras.\n\n";
+
+    char figura;
+    do {
+        cout << "Escoja una figura:\n";
+        cout << "  a. Cilindro\n";
+        cout << "  b. Esfera\n";
+        cout << "  c. Prisma rectangular\n";
+        cout << "Selección: ";
+        cin >> figura;
+    } while (figura != 'a' && figura != 'b' && figura != 'c');
+
+    char calculo;
+    do {
+        cout << "\nEscoja entre los siguientes opciones:\n";
+        cout << "  a. Volumen\n";
+        cout << "  b. Área de la superficie\n";
+        cout << "Selección: ";
+        cin >> calculo;
+    } while (calculo != 'a' && calculo != 'b');
+
+    float radio, altura, largo, ancho;
+
+    switch (figura) {
+        case 'a': // Cilindro
+            do {
+                cout << "Entre el radio del cilindro: ";
+                cin >> radio;
+            } while (radio < 0);
+            do {
+                cout << "Entre la altura del cilindro: ";
+                cin >> altura;
+            } while (altura < 0);
+
+            if (calculo == 'a')
+                cout << "El volumen del cilindro es " << calcular(radio, altura, true) << endl;
+            else
+                cout << "El área del cilindro es " << area(radio, altura, true) << endl;
+            break;
+
+        case 'b': // Esfera
+            do {
+                cout << "Entre el radio de la esfera: ";
+                cin >> radio;
+            } while (radio < 0);
+
+            if (calculo == 'a')
+                cout << "El volumen de la esfera es " << calcular(radio, 'e') << endl;
+            else
+                cout << "El área de la esfera es " << area(radio, 'e') << endl;
+            break;
+
+        case 'c': // Prisma
+            do {
+                cout << "Entre el largo del prisma: ";
+                cin >> largo;
+            } while (largo < 0);
+            do {
+                cout << "Entre el ancho del prisma: ";
+                cin >> ancho;
+            } while (ancho < 0);
+            do {
+                cout << "Entre la altura del prisma: ";
+                cin >> altura;
+            } while (altura < 0);
+
+            if (calculo == 'a')
+                cout << "El volumen del prisma es " << calcular(largo, ancho, altura) << endl;
+            else
+                cout << "El área del prisma es " << area(largo, ancho, altura) << endl;
+            break;
+    }
+
+    return 0;
+}
